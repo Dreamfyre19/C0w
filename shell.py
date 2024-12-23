c5a4832f71db02562c8c822801c29713bed914f8
Dreamfyre19 <ianduong19@icloud.com>
2024-12-23 17:09:25 -0600

    Add files via upload

--- shell.py
+++ shell.py
@@ -1,8 +1,8 @@
 import C0w
 
 while True:
-		text = input('C0w >> ')
-		result, error = C0w.run('<stdin>', text)
+	text = input('C0w >> ')
+	result, error = C0w.run('<stdin>', text)
 
-		if error: print(error.as_string())
-		else: print(result)
+	if error: print(error.as_string())
+	else: print(result)
